e1e712163398b6cda8e4e1635c48f8f5380393cd
EverVino <evervino00@gmail.com>
2023-11-06 18:13:46 -0400

    add docstring to hermite functions

--- src/fqlearn/hermite.py
+++ src/fqlearn/hermite.py
@@ -2,26 +2,120 @@ import math
 
 import numpy as np
 
-
-def h00(t):
-    return 2 * t**3 - 3 * t**2 + 1
-
-
-def h10(t):
-    return t**3 - 2 * t**2 + t
-
-
-def h01(t):
-    return -2 * t**3 + 3 * t**2
+"""
+These are functions that are used to perform Hermite's 
+monotonic cubic interpolation.
+For more info check 
+https://en.wikipedia.org/wiki/Monotone_cubic_interpolation
+"""
 
 
-def h11(t):
-    return t**3 - t**2
+def h00(t: float) -> float:
+    """
+    First Hermite monotonic cubic interpolation polynomial.
+
+    Parameters
+    ----------
+    t : float
+        Independent variable.
+
+    Returns
+    ------
+    h00 : float
+        returns 2t^3 - 3t^2 + 1
+        evaluated at t.
+    """
+    h00 = 2 * t**3 - 3 * t**2 + 1
+
+    return h00
 
 
+def h10(t: float) -> float:
+    """
+    Second Hermite monotonic cubic interpolation polynomial.
+
+    Parameters
+    ----------
+    t : float
+        Independent variable.
+
+    Returns
+    ------
+    h10 : float
+        returns t^3 - 2t^2 + t
+        evaluated at t.
+    """
+
+    h10 = t**3 - 2 * t**2 + t
+
+    return h10
+
+
+def h01(t: float) -> float:
+    """
+    Third Hermite monotonic cubic interpolation polynomial.
+
+    Parameters
+    ----------
+    t : float
+        Independent variable.
+
+    Returns
+    ------
+    h01 : float
+        returns -2t^3 + 3t^2
+        evaluated at t.
+    """
+
+    h01 = -2 * t**3 + 3 * t**2
+    return h01
+
+
+def h11(t: float) -> float:
+    """
+    Fourth Hermite monotonic cubic interpolation polynomial.
+
+    Parameters
+    ----------
+    t : float
+        Independent variable
+
+    Returns
+    -------
+    h11 : float
+        returns t^3 - t^2
+        evaluated at t
+    """
+
+    h11 = t**3 - t**2
+    return h11
+
+
+# TODO: Complete docstring check numpy arrays type
 def pchint(x, y, x0):
+    """
+    An python code to implement Hermite monotonic cubic interpolation.
+
+    Parameters
+    ----------
+    x0 : List[float]
+        List of values to interpolate.
+    x : List [float]
+        List of values in x axis used to perform interpolation
+    y : List[float]
+        List of values in y axis used to perform interpolation
+
+    Returns
+    -------
+    y : List[float]
+        A list of interpolated values
+    """
+
     n = len(x)
     d = np.zeros(n - 1)
+
+    # Calculating the first numerical derivatives
+    # store it in `d`
     for i in range(n - 1):
         d[i] = (y[i + 1] - y[i]) / (x[i + 1] - x[i])
 
@@ -45,18 +139,18 @@ def pchint(x, y, x0):
             alfa = m[i] / d[i]
             beta = m[i + 1] / d[i]
 
-            condicion = alfa - ((2 * alfa + beta - 3) ** 2) / (alfa + beta - 2) / 3
+            condition = alfa - ((2 * alfa + beta - 3) ** 2) / (alfa + beta - 2) / 3
 
-            while condicion < 0:
+            while condition < 0:
                 tau = 3 / math.sqrt(alfa**2 + beta**2)
                 alfa = tau * alfa
                 beta = tau * beta
                 m[i] = alfa * d[i]
                 m[i + 1] = beta * d[i]
-                condicion = alfa - ((2 * alfa + beta - 3) ** 2) / (alfa + beta - 2) / 3
+                condition = alfa - ((2 * alfa + beta - 3) ** 2) / (alfa + beta - 2) / 3
     pos = 0
     c = 0
-    F = np.zeros(len(x0))
+    y = np.zeros(len(x0))
 
     for xi in x0:
         if xi > x[pos + 1]:
@@ -64,11 +158,12 @@ def pchint(x, y, x0):
 
         delta = x[pos + 1] - x[pos]
         t = (xi - x[pos]) / delta
-        F[c] = (
+        y[c] = (
             y[pos] * h00(t)
             + delta * m[pos] * h10(t)
             + y[pos + 1] * h01(t)
             + delta * m[pos + 1] * h11(t)
         )
         c += 1
-    return F
+
+    return y
