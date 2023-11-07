17b7d10ac573f8fce91b3bc241688656f990c346
José María García Márquez <30852961+JoseMariaGarciaMarquez@users.noreply.github.com>
2023-11-07 07:01:11 -0600

    docs:: Adding jupyter notebooks to codin researhce MCabe-Thiele (#16)
    * utils.hermite
    
    * Minnior changes McCabe
    
    * Comments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fqlearn import McCabeThiele
 
 model = McCabeThiele()
-model.set_data(compound_a="methanol", compound_b="water")
+model.set_data(compound_a="hexano", compound_b="xileno")
 model.fit(xF=0.5, xD=0.94, xW=0.05)
 model.plot()
