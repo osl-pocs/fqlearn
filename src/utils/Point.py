3dcbcd4ed12a5b3af1b1f3d5cf9860a75551944e
José María García Márquez <30852961+JoseMariaGarciaMarquez@users.noreply.github.com>
2023-11-17 20:05:54 -0600

    docs: Adding features to McCabe-Thiele (#23)
    * pandas change McCabe-Thiele
    
    * describe function
    
    * Adding features
    
    * Valid inputs q & xF
    
    * Validating inputs

--- src/utils/Point.py
+++ src/utils/Point.py
@@ -1,7 +1,8 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
+import numpy as np
 from scipy.interpolate import interp1d
+from scipy.optimize import fsolve
+
 
 class Point:
     def __init__(self, df):
@@ -9,14 +10,13 @@ class Point:
 
     def create_feed_line(self, q, xf):
         if q == 1:
-            raise Exception('Not implemented yet') #To do list
-        return lambda x: q*x/(q-1) - xf/(q-1)
-
+            raise Exception("Not implemented yet")  # To do list
+        return lambda x: q * x / (q - 1) - xf / (q - 1)
 
     def find_intersection(self, q, xf, epsilon=1e-5):
         feed = self.create_feed_line(q, xf)
-        x_values = self.df['x'].values
-        y_values = self.df['y'].values
+        x_values = self.df["x"].values
+        y_values = self.df["y"].values
 
         def difference_function(x):
             return y_values - feed(x)
@@ -25,16 +25,20 @@ class Point:
         sign_changes = np.where(np.diff(np.sign(difference_function(x_values))))[0]
 
         if len(sign_changes) == 0:
-            return None 
+            return None
 
         intersections = []
 
         # Utiliza interpolación para encontrar la intersección en cada subintervalo
         for i in sign_changes:
-            x_interval = x_values[i:i+2]
-            y_interval = y_values[i:i+2]
-            interpolator = interp1d(x_interval, y_interval, kind='linear', fill_value="extrapolate")
-            intersection = fsolve(lambda x: interpolator(x) - feed(x), np.mean(x_interval))
+            x_interval = x_values[i : i + 2]
+            y_interval = y_values[i : i + 2]
+            interpolator = interp1d(
+                x_interval, y_interval, kind="linear", fill_value="extrapolate"
+            )
+            intersection = fsolve(
+                lambda x: interpolator(x) - feed(x), np.mean(x_interval)
+            )
             intersections.extend(intersection)
 
         return intersections
@@ -42,22 +46,26 @@ class Point:
     def plot_intersection(self, q_guess, xf_guess):
         intersections = self.find_intersection(q_guess, xf_guess)
 
-       
-        x_values = self.df['x'].values
-        y_values = self.df['y'].values
+        x_values = self.df["x"].values
+        y_values = self.df["y"].values
 
         feed = self.create_feed_line(q_guess, xf_guess)
         y_feed = feed(x_values)
-        
+
         print(intersections, feed(np.array(intersections)))
 
-        plt.plot(x_values, y_values, label='Línea de equilibrio')
-        plt.plot(x_values, y_feed, label='Feed line')
-        plt.scatter(intersections, feed(np.array(intersections)), color='red', label='Intersección')
-        plt.xlabel('x')
-        plt.ylabel('y')
+        plt.plot(x_values, y_values, label="Línea de equilibrio")
+        plt.plot(x_values, y_feed, label="Feed line")
+        plt.scatter(
+            intersections,
+            feed(np.array(intersections)),
+            color="red",
+            label="Intersección",
+        )
+        plt.xlabel("x")
+        plt.ylabel("y")
         plt.legend()
         plt.show()
-        
+
         return (intersections, feed(np.array(intersections)))
 
